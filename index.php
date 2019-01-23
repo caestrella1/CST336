8784ca36587a0478eabc5f345f886b8ae3fe4b4a
Carlos Estrada <caestrada@csumb.edu>
2019-01-23 01:31:11 +0000

    Added index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+    <?php
+    // A simple web site in Cloud9 that runs through Apache
+    // Press the 'Run' button on the top to start the web server,
+    // then click the URL that is emitted to the Output tab of the console
+    
+    echo "Hello world!!!!";
+    
+    ?>
+    <div>
+        <h1>Hello world!</h1>
+    </div>
+    </body>
+</html>
