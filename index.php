01f0938a6469f39a4e9ae2db4589ef69561e5709
Carlos Estrada <caestrada@csumb.edu>
2019-01-23 01:34:29 +0000

    Test

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <html>
     <body>
-    <?php
-    // A simple web site in Cloud9 that runs through Apache
-    // Press the 'Run' button on the top to start the web server,
-    // then click the URL that is emitted to the Output tab of the console
-    
-    echo "Hello world!!!!";
-    
-    ?>
-    <div>
-        <h1>Hello world!</h1>
-    </div>
+        <div>
+            <h1>Hello world!</h1>
+            <p>Bottom text</p>
+        </div>
     </body>
 </html>
