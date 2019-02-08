9b77fb878c119c1c9b4624048a2ceb46be20e4da
Carlos Estrada <cestrrada@gmail.com>
2019-02-08 19:42:04 +0000

    Made root index nice and useful.

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
+<!DOCTYPE html>
 <html>
-    <body>
-        <div>
-            <h1>Hello world!</h1>
-            <p>Bottom text</p>
+    <head>
+        <title>Hello World</title>
+        <meta charset="utf-8"></meta>
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+    </head>
+    <body id="container">
+        <div id="content">
+            <h1>Hello world.</h1>
+            <p>Welcome to my repo for <strong>CST 336: Internet Programming.</strong></p>
+            <p>Browse all projects and assignments on my GitHub.</p>
+            <a id="github" href="https://github.com/cestrrada/CST336" target="_blank">GitHub</a>
         </div>
     </body>
 </html>
