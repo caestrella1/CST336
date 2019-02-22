3c0666b0d2853e1658c8bf0518bc999020e4407a
Carlos Estrada <cestrrada@gmail.com>
2019-02-22 03:53:18 +0000

    Started Lab 4

--- labs/lab4/js/functions.js
+++ labs/lab4/js/functions.js
@@ -1,6 +1,27 @@
 /* global $ */
 
 $(function() {
+    $("#zip-error").hide();
+    
+    $("#state").on("change", function() {
+        $.ajax({
+            type: "GET",
+            url: "http://itcdland.csumb.edu/~milara/ajax/countyList.php",
+            dataType: "json",
+            data: { "state": $("#state").val() },
+            success: function(data,status) {
+                $("#county").html("Select one");
+                for (let i = 0; i < data.length; i++) {
+                    $("#county").append("<option>" + data[i].county + "</option>")
+                }
+            },
+            complete: function(data,status) { //optional, used for debugging purposes
+            // alert(status);
+            }
+        
+        });//ajax
+    });
+    
     $("#zip").on("change", function() {
         $.ajax({
             type: "GET",
@@ -8,13 +29,53 @@ $(function() {
             dataType: "json",
             data: { "zip": $("#zip").val() },
             success: function(data,status) {
-            alert(data.city);
-            
+                if (typeof data["zip"] === "undefined") {
+                    $("#zip-error").show();
+                    $("#city").html("");
+                    $("#lat").html("");
+                    $("#long").html("");
+                }
+                else {
+                    $("#zip-error").hide();
+                    $("#city").html(data.city);
+                    $("#lat").html(data.latitude);
+                    $("#long").html(data.longitude);
+                }
+            },
+            error: function() {
+                $("#zip-error").show();
             },
             complete: function(data,status) { //optional, used for debugging purposes
             // alert(status);
             }
         
         });//ajax
+    });
+    
+    $("#username").on("change", function() {
+       $.ajax({
+            type: "GET",
+            url: "http://myspace.csumb.edu/~milara/ajax/username/usernameLookup.php",
+            dataType: "json",
+            data: { "username": $("#username").val() },
+            success: function(data,status) {
+                // alert(data.username);
+                if (data.available == "true") {
+                    $("#username-feedback").html("Username is available");
+                    $("#username-feedback").addClass("text-success").removeClass("text-danger");
+                    // alert("available");
+                }
+                else {
+                    $("#username-feedback").html("Username is not available");
+                    $("#username-feedback").addClass("text-danger").removeClass("text-success");
+                }
+            },
+            error: function(data, status) {
+                
+            },
+            complete: function(data,status) { //optional, used for debugging purposes
+            
+            }
+       });
     });
 });
